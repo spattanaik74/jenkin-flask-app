179828655942005d4b41edaa92b436691d90d076
Sarthak Pattanaik <spattanaik74@gmail.com>
2026-06-12 11:44:48 +0530

    test45645

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"status": "success", "message": "Hello Jenkins CI/CD/PRODUCTION-TEST-RECORD-123"})
+    return jsonify({"status": "success", "message": "Hello Jenkins PROD_TEST_123"})
 
 
 if __name__ == "__main__":
